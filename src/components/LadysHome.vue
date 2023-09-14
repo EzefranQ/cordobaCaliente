198475a0556674659ebfef0d06f6b570fb7d04d5
Your Name <you@example.com>
2023-09-14 12:48:22 -0400

    sad commit

--- src/components/LadysHome.vue
+++ src/components/LadysHome.vue
@@ -1,12 +1,39 @@
 <template>
     <div class="flex flex-wrap md:justify-center justify-between md:gap-20 gap-10 -translate-y-14 z-50 relative sm:px-10 px-4">
-        <article v-for="lady in ladysArr" :id="lady.id" class="m-auto border-2 border-black border-opacity-10">
+        <article class="m-auto border-2 border-black border-opacity-10">
             <div class="sm:w-[300px] h-[400px] md:w-[250px] m-auto">
-                <img :src="lady.src" alt="" class="w-full h-full object-cover" width="100%" height="100%">
+                <img src="../assets/img/lady.jpg" alt="" class="w-full h-full object-cover" width="100%" height="100%">
             </div>
             <div class="text-center p-4">
-                <p class="text-xl">{{ lady.name }}</p>
-                <p class="text-lg">{{ lady.ubication }}</p>
+                <p class="text-xl">Mamasita hot argentina</p>
+                <p class="text-lg">Córdoba</p>
+            </div>
+        </article>
+        <article class="m-auto border-2 border-black border-opacity-10">
+            <div class="sm:w-[300px] h-[400px] md:w-[250px] m-auto">
+                <img src="../assets/img/lady2.jpg" alt="" class="w-full h-full object-cover" width="100%" height="100%">
+            </div>
+            <div class="text-center p-4">
+                <p class="text-xl">Mamasita hot argentina</p>
+                <p class="text-lg">Córdoba</p>
+            </div>
+        </article>
+        <article class="m-auto border-2 border-black border-opacity-10">
+            <div class="sm:w-[300px] h-[400px] md:w-[250px] m-auto">
+                <img src="../assets/img/lady.jpg" alt="" class="w-full h-full object-cover" width="100%" height="100%">
+            </div>
+            <div class="text-center p-4">
+                <p class="text-xl">Mamasita hot argentina</p>
+                <p class="text-lg">Córdoba</p>
+            </div>
+        </article>
+        <article class="m-auto border-2 border-black border-opacity-10">
+            <div class="sm:w-[300px] h-[400px] md:w-[250px] m-auto">
+                <img src="../assets/img/lady2.jpg" alt="" class="w-full h-full object-cover" width="100%" height="100%">
+            </div>
+            <div class="text-center p-4">
+                <p class="text-xl">Mamasita hot argentina</p>
+                <p class="text-lg">Córdoba</p>
             </div>
         </article>
     </div>
